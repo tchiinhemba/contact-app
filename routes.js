6c42ee4a1e77dcfef3d39de04c7d1ea53f0b98b6
eladioclaudio <elladioclaudio@gmail.com>
2022-03-19 13:46:17 +0100

    sign in and sign up page

--- routes.js
+++ routes.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const route = express.Router();
+
+
 const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
 
 // Rotas da home
 route.get('/', homeController.index);
+
+
+// Login Routes
+route.get('/login/index', loginController.index);
 
 
 module.exports = route;
