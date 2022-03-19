5c61ce8f5970bea9e09e328202f7938e2a1abaac
eladioclaudio <elladioclaudio@gmail.com>
2022-03-19 14:07:37 +0100

    auto commit

--- routes.js
+++ routes.js
@@ -11,7 +11,7 @@ route.get('/', homeController.index);
 
 // Login Routes
 route.get('/login/index', loginController.index);
-route.post('/login/register', loginController)
+route.post('/login/register', loginController.register)
 
 
 module.exports = route;
