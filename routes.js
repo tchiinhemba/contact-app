f5f770436ace75890bb2fd67d6268d49a164f121
tchiinhemba <tchiinhemba@outlook.com>
2022-03-23 23:50:12 +0100

    create a contact route...

--- routes.js
+++ routes.js
@@ -2,8 +2,14 @@ const express = require('express');
 const route = express.Router();
 
 
+// Controllers
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contactController = require('./src/controllers/contactController');
+
+
+// Middlewars
+const { loginRequired } = require('./src/middlewares/middleware')
 
 // Rotas da home
 route.get('/', homeController.index);
@@ -13,6 +19,12 @@ route.get('/', homeController.index);
 route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
+
+
+// Contact Routes
+route.get('/contact/index', loginRequired, contactController.index);
+route.post('/contact/register', loginRequired, contactController.register)
 
 
 module.exports = route;
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,6 +1,7 @@
 exports.middlewareGlobal = (req, res, next) => {
     res.locals.errors = req.flash('Errors');
     res.locals.success = req.flash('Success');
+    res.locals.user = req.session.user;
     next();
   };
   
@@ -19,3 +20,16 @@ exports.middlewareGlobal = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
     next();
   };
+
+
+
+  exports.loginRequired = (req, res, next) => {
+
+    if(!req.session.user) {
+      req.flash('errors', 'You need login.')
+      req.session.save((() => { res.redirect('/')}));
+      return;
+    }
+
+    next();
+  }
