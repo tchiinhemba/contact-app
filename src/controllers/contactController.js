08a3bd22f5214ac3e5579aa943a9149f2d4fb3cf
tchiinhemba <tchiinhemba@outlook.com>
2022-03-24 17:17:48 +0100

    create read crud operation

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -2,7 +2,9 @@
 const Contact = require('../models/ContactModel');
 
 exports.index = (req, res) => {
-    res.render('contact')
+    res.render('contact', {
+        contact: {}
+    })
 }
 
 exports.register = async (req, res) => {
@@ -12,13 +14,13 @@ exports.register = async (req, res) => {
 
 
         if (contact.errors.length > 0) {
-            req.flash('errors', contact.errors);
-            req.session.save(() => { res.redirect('back') })
+            req.flash('Errors', contact.errors);
+            req.session.save(() => { res.redirect('/contact/index') })
             return
         }
 
 
-        req.flash('success', 'Contact are successfully created!');
+        req.flash('Success', 'Contact are successfully created!');
         req.session.save(() => res.redirect(`/contact/index/${contact.contact._id}`))
         return;
     } catch (error) {
@@ -29,9 +31,37 @@ exports.register = async (req, res) => {
 
 
 exports.editIndex = async (req, res) => {
-    if(!req.params.id) return res.render('404');
+    if (!req.params.id) return res.render('404');
 
     const contact = await Contact.searchForId(req.params.id);
-    if(!contact) return res.render('404')
+    if (!contact) return res.render('404')
     res.render('contact', { contact });
+}
+
+
+exports.edit = async (req, res) => {
+
+    try {
+        if (!req.params.id) return res.render('404');
+        const contact = new Contact(req.body);
+        await contact.edit(req.params.id);
+
+
+        if (contact.errors.length > 0) {
+            req.flash('Errors', contact.errors);
+            req.session.save(() => { res.redirect('/contact/index') })
+            return
+        }
+
+
+        req.flash('Success', 'Contact are successfully Edit!');
+        req.session.save(() => res.redirect(`/contact/index/${contact.contact._id}`))
+        return;
+
+    } catch (error) {
+        console.log(error)
+        return res.render('404')
+    }
+
+
 }
